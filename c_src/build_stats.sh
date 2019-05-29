0fd9bb8c2897570fb33a1e3da6087e7b86fd7f3e
Andrew Thompson <andrew@hijacked.us>
2019-05-28 17:11:11 -0700

    Pin dep to 2.2.0

--- c_src/build_stats.sh
+++ c_src/build_stats.sh
@@ -5,4 +5,7 @@ if [ ! -d stats ]; then
     git clone --recursive https://github.com/kthohr/stats
 fi
 
+# pinned to 2.2.0 commit, no tag
+(cd stats ; git reset --hard 659b1d9af2208de32fa17bcee9e4f3b0c5063a22)
+
 make
