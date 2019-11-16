f9c64d04a043e7ce234c6bb3b1b346fa4247e145
Andrew Thompson <andrew@hijacked.us>
2019-11-16 08:34:06 -0800

    Upgrade stats library to 3.0.0 and add gcem dependency

--- c_src/build_stats.sh
+++ c_src/build_stats.sh
@@ -5,7 +5,14 @@ if [ ! -d stats ]; then
     git clone --recursive https://github.com/kthohr/stats
 fi
 
-# pinned to 2.2.0 commit, no tag
-(cd stats ; git reset --hard 659b1d9af2208de32fa17bcee9e4f3b0c5063a22)
+# pinned to 3.0.0 merge commit, no tag
+(cd stats ; git reset --hard 8545bf8713d9529baa7b2e0c8eed23e6e93b9039)
+
+if [ ! -d gcem ]; then
+    git clone --recursive https://github.com/kthohr/gcem
+fi
+
+# HEAD of gcem rougly contemperaneous with stats 3.0.0 merge
+(cd gcem ; git reset --hard 94443c99a7db939ac2edbba2674fdc54a8ec70c5)
 
 make
